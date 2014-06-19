ba6aa0f090523dfc5675e553c53d846719ed91c2
Todd Wolfson <todd@twolfson.com>
2014-06-19 02:24:43 -0700

    Added test script

--- test/tab-completion_test.js
+++ test/tab-completion_test.js
@@ -2,7 +2,8 @@ var tab_completion = require('../');
 
 describe('tab-completion', function () {
   before(function () {
-
+    // http://stackoverflow.com/a/9505024/1960509
+    // COMP_LINE="foundry re" COMP_WORDS=(foundry re) COMP_CWORD=1 COMP_POINT=10 $(complete -p foundry | sed "s/.*-F \\([^ ]*\\) .*/\\1/") && echo ${COMPREPLY[*]}
   });
 
   it('', function () {
