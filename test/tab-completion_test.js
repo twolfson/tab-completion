8460d98a38a532ae989991a26c6e0f8881bdda3c
Todd Wolfson <todd@twolfson.com>
2014-06-21 04:50:22 -0700

    Updated comments

--- test/tab-completion_test.js
+++ test/tab-completion_test.js
@@ -1,9 +1,12 @@
-var tab_completion = require('../');
+// Load dependencies
+var TabCompletion = require('../');
 
-if (process.env.VAGRANT !== 'true' || process.env.TRAVIS !== 'true') {
+// Verify we are in a contained environment
+if (process.env.VAGRANT !== 'true' && process.env.TRAVIS !== 'true') {
   throw new Error('Tests must be run in enclosed environment (e.g. Vagrant, Travis) to prevent accidents on host OS');
 }
 
+// Start our tests
 describe('tab-completion', function () {
   before(function () {
     // http://stackoverflow.com/a/9505024/1960509
