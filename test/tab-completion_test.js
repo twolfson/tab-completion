42fa5899baf40fa74e9486ce5963487dbf3c99a6
Todd Wolfson <todd@twolfson.com>
2014-06-21 05:40:28 -0700

    Set up test outlines

--- test/tab-completion_test.js
+++ test/tab-completion_test.js
@@ -8,12 +8,53 @@ if (process.env.VAGRANT !== 'true' && process.env.TRAVIS !== 'true') {
 
 // Start our tests
 describe('tab-completion', function () {
-  before(function () {
-    // http://stackoverflow.com/a/9505024/1960509
-    // COMP_LINE="foundry re" COMP_WORDS=(foundry re) COMP_CWORD=1 COMP_POINT=10 $(complete -p foundry | sed "s/.*-F \\([^ ]*\\) .*/\\1/") && echo ${COMPREPLY[*]}
+  describe.skip('installed on a bash system', function () {
+    it('adds the script to the .bashrc file', function () {
+
+    });
+
+    describe('when completed against', function () {
+      // http://stackoverflow.com/a/9505024/1960509
+      // COMP_LINE="foundry re" COMP_WORDS=(foundry re) COMP_CWORD=1 COMP_POINT=10 $(complete -p foundry | sed "s/.*-F \\([^ ]*\\) .*/\\1/") && echo ${COMPREPLY[*]}
+
+      it('runs our completion command', function () {
+
+      });
+    });
+
+    describe('when uninstalled', function () {
+      it('leaves the .bashrc like it found it', function () {
+
+      });
+    });
   });
 
-  it('', function () {
+  describe.skip('installed on a zsh system', function () {
+    it('adds the script to the .zsh file', function () {
 
+    });
+
+    describe('when completed against', function () {
+      it('runs our completion command', function () {
+
+      });
+    });
+
+    describe('when uninstalled', function () {
+      it('leaves the .zsh like it found it', function () {
+
+      });
+    });
+
+  });
+
+  describe.skip('installed on a non-bash/zsh system', function () {
+    it('does not generate a .bashrc file', function () {
+
+    });
+
+    it('does not generate a .zshrc file', function () {
+
+    });
   });
 });
