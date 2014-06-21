ad98dcc749b4676a30d2de02ddbd5bfbab86b623
Todd Wolfson <todd@twolfson.com>
2014-06-21 05:49:01 -0700

    Defined fs utils for tests

---
+++ test/utils/fs.js
@@ -0,0 +1,23 @@
+// Load in dependencies
+var cp = require('cp');
+
+// Define common fs helpers
+exports.cp = function (src, dest) {
+  before(function cpFn (done) {
+    cp(src, dest, done);
+  });
+};
+
+exports.readFile = function (filepath) {
+  before(function readFileFn (done) {
+    // Load the file into memory and callback with any errors
+    var that = this;
+    fs.readFile(filepath, function handleFileRead (err, buff) {
+      that.buff = buff;
+      done(err);
+    });
+  });
+  after(function cleanupFileFn () {
+    delete this.buff;
+  });
+};
