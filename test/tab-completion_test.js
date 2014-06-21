ad4b7d36d0efd6e42bb7762576d2d6a3e7e8fab4
Todd Wolfson <todd@twolfson.com>
2014-06-21 04:49:13 -0700

    Adding safety net for tests

--- test/tab-completion_test.js
+++ test/tab-completion_test.js
@@ -1,5 +1,9 @@
 var tab_completion = require('../');
 
+if (process.env.VAGRANT !== 'true' || process.env.TRAVIS !== 'true') {
+  throw new Error('Tests must be run in enclosed environment (e.g. Vagrant, Travis) to prevent accidents on host OS');
+}
+
 describe('tab-completion', function () {
   before(function () {
     // http://stackoverflow.com/a/9505024/1960509
